72286ddec53de461e94e83c7ad46a5d67383dab9
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-15 09:15:49 -0300

    dividido tabela em componentes de corpo e cabecao

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,54 +1,15 @@
 import React from 'react';
 import styles from '../styles/Table.module.css';
+import TableBody from './TableBody';
+import TableHeader from './TableHeader';
 
 function Table() {
   return (
     <table className={ styles.table__container }>
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Rotation Period
-          </th>
-          <th>
-            Orbital Period
-          </th>
-          <th>
-            Diameter
-          </th>
-          <th>
-            Climate
-          </th>
-          <th>
-            Gravity
-          </th>
-          <th>
-            Terrain
-          </th>
-          <th>
-            Surface Water
-          </th>
-          <th>
-            Population
-          </th>
-          <th>
-            Films
-          </th>
-          <th>
-            Created
-          </th>
-          <th>
-            Edited
-          </th>
-          <th>
-            URL
-          </th>
-        </tr>
-      </thead>
+      <TableHeader />
+      <TableBody />
     </table>
   );
-};
+}
 
 export default Table;
---
+++ src/components/TableBody.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { Context } from '../context/StarwarsProvider';
+
+function TableBody() {
+  const { data } = useContext(Context);
+
+  function filmes(name, filmeList) {
+    return (
+      <ul>
+        {filmeList.map((film, index) => (
+          <li key={ `${name}-${index}` }>{film}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <tbody>
+      { data.map(({
+        name,
+        rotation_period: rotation,
+        orbital_period: orbital,
+        diameter,
+        climate,
+        gravity,
+        terrain,
+        surface_water: surfaceWater,
+        population,
+        films,
+        created,
+        edited,
+        url,
+      }) => (
+        <tr key={ name }>
+          <td>{ name }</td>
+          <td>{ rotation }</td>
+          <td>{ orbital }</td>
+          <td>{ diameter }</td>
+          <td>{ climate }</td>
+          <td>{ gravity }</td>
+          <td>{ terrain }</td>
+          <td>{ surfaceWater }</td>
+          <td>{ population }</td>
+          <td>{ filmes(name, films) }</td>
+          <td>{ created }</td>
+          <td>{ edited }</td>
+          <td>{ url }</td>
+        </tr>
+      )) }
+    </tbody>
+  );
+}
+
+export default TableBody;
