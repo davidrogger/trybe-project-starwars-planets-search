5fc6a49743fa5fc877c41b2ffbbec75a6898a61c
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-15 09:34:03 -0300

    criado componente para input de filtros da tabela

---
+++ src/components/FilterPainel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/FilterPainel.module.css';
+
+function FilterPainel() {
+  return (
+    <div className={ styles.painel__container }>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default FilterPainel;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from '../styles/Table.module.css';
+import FilterPainel from './FilterPainel';
 import TableBody from './TableBody';
 import TableHeader from './TableHeader';
 
 function Table() {
   return (
-    <table className={ styles.table__container }>
-      <TableHeader />
-      <TableBody />
-    </table>
+    <div>
+      <FilterPainel />
+      <table className={ styles.table__container }>
+        <TableHeader />
+        <TableBody />
+      </table>
+    </div>
   );
 }
 
