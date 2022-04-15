b3dcdf86925eecfe275b8901732c0a0fb0904afd
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-15 07:53:18 -0300

    criada funcao que recebe informacao da api

---
+++ src/services/starwarsAPI.js
@@ -0,0 +1,9 @@
+const URL = 'https://swapi-trybe.herokuapp.com/api/planets';
+
+const getStarWarsPlanets = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+};
+
+export default getStarWarsPlanets;
