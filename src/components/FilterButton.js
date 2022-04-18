a3f4f6f2c73810138dbe4f238b11d5e78b65957e
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-18 09:44:22 -0300

    implementada acao de clique ao filtro numerico no componente

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Context } from '../context/StarwarsProvider';
 
-function FilterButton() {
+function FilterButton({ testeId }) {
+  const { filterByNumericValue, setfilterResultNumeric } = useContext(Context);
+
+  function numericFilter() {
+    const { column, comparison, value } = filterByNumericValue;
+
+    const hexadecimal = 16;
+    const id = `id${Math.random().toString(hexadecimal).slice(2)}`; // https://stackoverflow.com/questions/3231459/create-unique-id-with-javascript
+    const newNumericFilter = { id, column, comparison, value };
+
+    setfilterResultNumeric((prevState) => ([
+      ...prevState,
+      newNumericFilter,
+    ]));
+  }
+
   return (
     <button
       type="button"
+      data-testid={ testeId }
+      onClick={ numericFilter }
     >
       Filtrar
     </button>
   );
 }
+
+FilterButton.propTypes = {
+  testId: PropTypes.string,
+}.isRequired;
 
 export default FilterButton;
