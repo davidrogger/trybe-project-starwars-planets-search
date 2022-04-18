1cbe7174dd53612f4fd716d8060d390255c4a6a0
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-18 19:28:48 -0300

    implementado botao para remover determinado filtro do botao

--- src/components/NumericFilterCard.js
+++ src/components/NumericFilterCard.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { Context } from '../context/StarwarsProvider';
 
 function NumericFilterCard({ column, comparison, value, id }) {
+  const { filterNumeric, setFilterNumeric } = useContext(Context);
+
+  function removeFilter(removeId) {
+    const newFilter = [...filterNumeric];
+    const indexId = newFilter.findIndex((removeItem) => removeItem.id === removeId);
+    newFilter.splice(indexId, 1);
+    setFilterNumeric(newFilter);
+  }
+
   return (
-    <li>
-      {`Coluna ${column} ${comparison} que ${value}, key ${id}`}
+    <li data-testid="filter">
+      {`Coluna ${column} ${comparison} que ${value}.`}
+      <button
+        type="button"
+        onClick={ () => removeFilter(id) }
+      >
+        X
+      </button>
     </li>
   );
 }
