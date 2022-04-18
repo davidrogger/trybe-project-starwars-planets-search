660e276f2a231d0fe10dd6ee2979e025b45dcdc6
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-18 19:36:46 -0300

    implementada botao de remover todos filtros do botao

--- src/components/FilterPainel.js
+++ src/components/FilterPainel.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../context/StarwarsProvider';
 import styles from '../styles/FilterPainel.module.css';
 import FilterByName from './FilterByName';
 import FilterByNumericValues from './FilterByNumericValues';
 
 function FilterPainel() {
+  const { setFilterNumeric } = useContext(Context);
+
   return (
     <div className={ styles.painel__container }>
       <div className={ styles.painel__head }>
@@ -12,6 +15,13 @@ function FilterPainel() {
       <div>
         <FilterByNumericValues />
       </div>
+      <button
+        type="button"
+        data-testid="button-remove-filters"
+        onClick={ () => setFilterNumeric([]) }
+      >
+        Remover todas filtragens
+      </button>
     </div>
   );
 }
