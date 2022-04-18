9a420e15345894162851d5f3556f221540e5f5f6
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-18 09:45:44 -0300

    implementado demonstracao de filtros que foram salvos no painel

--- src/components/FilterByNumericValues.js
+++ src/components/FilterByNumericValues.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../context/StarwarsProvider';
 import FilterButton from './FilterButton';
 import InputSelectColumn from './InputSelectColumn';
 import InputSelectComparison from './InputSelectComparison';
 import InputValueFilter from './InputValueFilter';
+import styles from '../styles/FilterPainel.module.css';
+import NumericFilterCard from './NumericFilterCard';
 
 function FilterByNumericValues() {
+  const { filterResultNumeric } = useContext(Context);
   return (
-    <div>
-      <InputSelectColumn />
-      <InputSelectComparison />
-      <InputValueFilter />
-      <FilterButton />
+    <div className={ styles.numericValues__container }>
+      <div className={ styles.numericValues__input }>
+        <InputSelectColumn />
+        <InputSelectComparison />
+        <InputValueFilter />
+        <FilterButton testId="value-filter" />
+      </div>
+      <div>
+        <ul>
+          { filterResultNumeric.map(({ column, comparison, value, id }) => (
+            <NumericFilterCard
+              key={ id }
+              id={ id }
+              column={ column }
+              comparison={ comparison }
+              value={ value }
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
