5971e46c41af5ad74640ef1fcaefdf06ff2f47b7
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-19 17:30:18 -0300

    adicionando classes ao inputradio e ajustando tamanho dos textos

--- src/components/InputRadioOrder.js
+++ src/components/InputRadioOrder.js
@@ -1,24 +1,27 @@
 import React, { useContext } from 'react';
 import { Context } from '../context/StarwarsProvider';
 import setState from '../helpers/setState';
+import styles from '../styles/FilterPainel.module.css';
 
 const radiosOptions = ['asc', 'desc'];
 
 function InputRadioOrder() {
   const { filterOrder, setFilterOrder } = useContext(Context);
   return (
-    <div>
+    <div className={ styles.inputRadio__container }>
       { radiosOptions.map((radio) => (
-        <input
-          key={ radio }
-          data-test-id={ `column-sort-input-${radio}` }
-          type="radio"
-          id={ radio }
-          name="sort"
-          value={ radio }
-          checked={ filterOrder.sort === radio }
-          onChange={ ({ target }) => setState(target, setFilterOrder) }
-        />
+        <label htmlFor={ radio } key={ radio }>
+          <input
+            data-test-id={ `column-sort-input-${radio}` }
+            type="radio"
+            id={ radio }
+            name="sort"
+            value={ radio }
+            checked={ filterOrder.sort === radio }
+            onChange={ ({ target }) => setState(target, setFilterOrder) }
+          />
+          { radio === 'asc' ? 'Ascendente' : 'Descendente' }
+        </label>
       )) }
     </div>
   );
