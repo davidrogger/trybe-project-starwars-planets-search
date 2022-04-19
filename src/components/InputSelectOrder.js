f6366be9baa73540285f795eda702b79bf3ddf30
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-19 14:56:39 -0300

    implementando inputselect no componente selectorder

--- src/components/InputSelectOrder.js
+++ src/components/InputSelectOrder.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../context/StarwarsProvider';
+import setState from '../helpers/setState';
+import InputSelect from './InputSelect';
 
 function InputSelectOrder() {
+  const { options, filterOrder, setFilterOrder } = useContext(Context);
   return (
-    <select>
-      
-    </select>
-  )
+    <InputSelect
+      testeId="column-sort"
+      name="column"
+      value={ filterOrder.column }
+      onChange={ ({ target }) => setState(
+        target, setFilterOrder,
+      ) }
+      options={ options }
+    />
+  );
 }
 
 export default InputSelectOrder;
