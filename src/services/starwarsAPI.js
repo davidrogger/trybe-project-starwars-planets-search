f605ce6775ea19743bdc34f7d1727f2f0db24ffe
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-03 08:28:35 -0300

    CHORE: added fake data api
    remote API isnt working

--- src/services/starwarsAPI.js
+++ src/services/starwarsAPI.js
@@ -1,9 +1,13 @@
+import starWarsPlanets from '../testData';
+
 const URL = 'https://swapi-trybe.herokuapp.com/api/';
 
-const getStarWarsAPI = async (endpoint) => {
+export const getStarWarsAPI = async (endpoint) => {
   const response = await fetch(`${URL}${endpoint}`);
   const data = await response.json();
   return data;
 };
 
-export default getStarWarsAPI;
+const getStarWarsData = async () => starWarsPlanets;
+
+export default getStarWarsData;
