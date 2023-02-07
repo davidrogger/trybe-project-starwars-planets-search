a5a49e14145d3d7b77f8213395721967259c550c
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-07 08:11:45 -0300

    Updates

---
+++ precache-manifest.71cce658ae5828430d64a952f796f2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff076a45f31f6a55b3fa7866f4a8af93",
+    "url": "/trybe-project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "e8ef0919f65bca74e225",
+    "url": "/trybe-project-starwars-planets-search/static/css/main.6afdab64.chunk.css"
+  },
+  {
+    "revision": "8224ff95a2d0de7e03e0",
+    "url": "/trybe-project-starwars-planets-search/static/js/2.4157fce0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trybe-project-starwars-planets-search/static/js/2.4157fce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ef0919f65bca74e225",
+    "url": "/trybe-project-starwars-planets-search/static/js/main.9aeb9edf.chunk.js"
+  },
+  {
+    "revision": "1efd97b390c4f7fc86aa",
+    "url": "/trybe-project-starwars-planets-search/static/js/runtime-main.1b33c893.js"
+  }
+]);
